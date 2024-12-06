8fab7c694b9aa38ef449b59823f4fb0145a29cc1
zafergunayonalan <zafergunay7@gmail.com>
2024-12-06 15:18:08 +0300

    Mens-created

--- src/Pages/Mens.jsx
+++ src/Pages/Mens.jsx
@@ -1,7 +1,26 @@
+import { useShopContext } from "../hooks/shopHooks"
+import banner from '../assets/MensBanner.png'
+import Item from "../components/Item";
 
 const Mens = () => {
+  const { allProduct } = useShopContext();
+  const menProduct = allProduct.filter((product) => product.category === "men")
   return (
-    <div>Mens</div>
+    <div className="md:mt-32 mt-20 max-w-7xl mx-auto">
+      <div>
+        <img src={banner} alt="" className="w-screen px-6"/>
+      </div>
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:pt-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tighter text-gray-900 text-center md:text-start">Men's Collection</h2>
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 px-6 md:px-0 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {
+            menProduct.map((product) => {
+              return <Item key={product.id} product={product}/>
+            })
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
   return (
     <div className="bg-white px-4 fixed w-full z-50 shadow-sm top-0 shadow-gray-400">
-      <div className="max-w-7xl mx-auto py-2 px-5 flex justify-between items-center">
+      <div className="max-w-screen-2xl mx-auto py-2 px-5 flex justify-between items-center">
         <img src={Logo} alt="" className="md:w-24 w-20" />
         <div className="flex items-center gap-5">
           <nav className="hidden md:block">
